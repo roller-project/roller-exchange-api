7cdd99395e0ba98ba6202372296342e2f8dc2445
Roller Davic <rollerdavic@gmail.com>
2018-08-17 21:00:31 +0700

    Update

--- application/config/private_api.php
+++ application/config/private_api.php
@@ -1,3 +1,4 @@
 <?php
 include_once __DIR__."/rest_zone.php";
-$config['rest_auth'] = TRUE;
+$config['rest_enable_keys'] = false;
+$config['rest_auth'] = false;
--- application/controllers/Privateapi.php
+++ application/controllers/Privateapi.php
@@ -19,5 +19,51 @@ class Privateapi extends API_Private {
 
 	}
 
+	public function buy_post(){
+		$trade = $this->input->post("trade");
+		list($symbol,$base) = explode('/', $trade);
+		if(!$symbol || !$base) {
+			$this->view(["error" => true,"msg" => "Symbol or basecoin Empty"]);
+			return;
+		}
+
+		$amount = $this->input->post("amount");
+		$prices = $this->input->post("prices");
+
+		$arv = [
+			"base" => $base,
+			"symbol" => $symbol,
+			"amount" => $amount,
+			"prices" => $prices,
+			"users_id" => "1",
+			"hash"		=>	sha1($amount.$prices)
+		];
+		$this->db->insert("trade_buy", $arv);
+		$this->view($arv);
+	}
+
+	public function sell_post(){
+		$trade = $this->input->post("trade");
+		list($symbol,$base) = explode('/', $trade);
+		if(!$symbol || !$base) {
+			$this->view(["error" => true,"msg" => "Symbol or basecoin Empty"]);
+			return;
+		}
+
+		$amount = $this->input->post("amount");
+		$prices = $this->input->post("prices");
+
+		$arv = [
+			"base" => $base,
+			"symbol" => $symbol,
+			"amount" => $amount,
+			"prices" => $prices,
+			"users_id" => "1",
+			"hash"		=>	sha1($amount.$prices)
+		];
+		$this->db->insert("trade_sell", $arv);
+		$this->view($arv);
+	}
+
 
 }
