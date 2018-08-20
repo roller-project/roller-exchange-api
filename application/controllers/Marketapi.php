0e6c001bd415befd440ff9a3954c40cf8d0ca55e
Roller Davic <rollerdavic@gmail.com>
2018-08-21 05:02:57 +0700

    Update

--- application/controllers/Marketapi.php
+++ application/controllers/Marketapi.php
@@ -214,6 +214,42 @@ class Marketapi extends API_Public {
 		return $arv;
 	}
 
+
+	private function array_sort($array, $on, $order=SORT_ASC)
+	{
+	    $new_array = array();
+	    $sortable_array = array();
+
+	    if (count($array) > 0) {
+	        foreach ($array as $k => $v) {
+	            if (is_array($v)) {
+	                foreach ($v as $k2 => $v2) {
+	                    if ($k2 == $on) {
+	                        $sortable_array[$k] = $v2;
+	                    }
+	                }
+	            } else {
+	                $sortable_array[$k] = $v;
+	            }
+	        }
+
+	        switch ($order) {
+	            case SORT_ASC:
+	                asort($sortable_array);
+	            break;
+	            case SORT_DESC:
+	                arsort($sortable_array);
+	            break;
+	        }
+
+	        foreach ($sortable_array as $k => $v) {
+	            $new_array[$k] = $array[$k];
+	        }
+	    }
+
+	    return $new_array;
+	}
+
 	private function renderChart($offsetTime, $period, $limit){
 		$arv = [];
 		
@@ -241,6 +277,6 @@ class Marketapi extends API_Public {
 				$arv[$end] = $obj;
 			
 		}
-		return $arv;
+		return $this->array_sort($arv,'openTime','SORT_ASC');
 	}
 }
