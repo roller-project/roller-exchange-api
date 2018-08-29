36baa049aa03d50bb592912502ce29adf7e9d558
macq@MacQs-MacBook.(none) <macq@MacQs-MacBook.local>
2018-08-29 08:24:21 +0700

    Update

--- application/models/Trade_model.php
+++ application/models/Trade_model.php
@@ -1,14 +1,17 @@
 <?php
 class Trade_Model extends DB_Model{
     private $login_id = 0;
+    public $return = [];
     function __construct()
     {
         parent::__construct();
         $this->login_id = $this->author_model->getLoginID();
 
     }
-    public $return = [];
-
+    
+    public function setLogin($id){
+        $this->login_id = $id;
+    }
     /*
 	Query Buy Task
     */
@@ -54,7 +57,9 @@ class Trade_Model extends DB_Model{
     	
     }
 
-
+    private function call_amount($amount, $prices){
+        return (float)$amount * (float)$prices;
+    }
 
     private function createTaskSell($base, $symbol, $amount, $prices, $type=""){
         $getBuyTask = $this->getBuyTask($base, $symbol, $prices);
@@ -65,12 +70,60 @@ class Trade_Model extends DB_Model{
         foreach ($getBuyTask as $key => $value) {
             if($amount > 0 && !$error){
                 if($value->amount > $amount){
-
+                    $updatedata = $this->updateBlancerBTC($this->login_id, $this->call_amount($amount,$prices), $value->users_id);
+                    if($updatedata == true){
+                        $this->db->delete("markets",["trade_id" => $value->trade_id]);
+                        $status = true;
+                    }else{
+                        $error = $updatedata;
+                    }
+                    $amount = 0;
                 }else{
+                    $amount = $amount - $value->amount;
 
+                    $arvs = [
+                        "amount" => $amount
+                    ];
+                    /*
+                    Update Market ID
+                    */
+                    $updatedata = $this->updateBlancerBTC($this->login_id, $this->call_amount($amount,$prices), $value->users_id);
+                    if($updatedata == true){
+                        $this->db->update("markets",$arvs,["trade_id" => $value->trade_id]);
+                        $status = true;
+                    }else{
+                        $error = $updatedata;
+                    }
+                    
                 }
             }
         }
+
+        /*
+        Create Task buy for member if not sell
+        */
+        $arv = [];
+        if($amount > 0 && !$error){
+            $arv = [
+                "trade_side" => "sell",
+                "trade_type" => "limit",
+                "base"      => $base,
+                "symbol"    =>  $symbol,
+                "prices"    => $prices,
+                "amount"    => $amount,
+                "users_id"  => $this->login_id,
+                "hash"      =>  sha1(time())
+            ];
+            if($this->blockTradeAltBalancer($symbol,$amount)){
+                $this->db->insert("markets", $arv);
+                 $status = true;
+            }else{
+                 $status = false;
+                 $error = $symbol." Blancer Empty";
+            }
+           
+        }
+        return ["status" => $status, "error" => $error];
     }
 
     /*
@@ -93,9 +146,9 @@ class Trade_Model extends DB_Model{
                     /*
                     Update Market ID
                     */
-                    $updatedata = $this->updateBlancerBTC($value->users_id, $amount * $prices, $this->login_id);
+                    $updatedata = $this->updateBlancerBTC($value->users_id, $this->call_amount($amount,$prices), $this->login_id);
                     if($updatedata == true){
-                        $this->db->update("markets",$arv,["trade_id" => $value->trade_id]);
+                        $this->db->update("markets",$arvs,["trade_id" => $value->trade_id]);
                         $status = true;
                     }else{
                         $error = $updatedata;
@@ -106,7 +159,7 @@ class Trade_Model extends DB_Model{
                     /*
                     Remove Markets
                     */
-                    $updatedata = $this->updateBlancerBTC($value->users_id, $value->amount * $prices, $this->login_id);
+                    $updatedata = $this->updateBlancerBTC($value->users_id, $this->call_amount($value->amount , $prices), $this->login_id);
                     if($updatedata == true){
                         $this->db->delete("markets",["trade_id" => $value->trade_id]);
                         $status = true;
@@ -146,6 +199,42 @@ class Trade_Model extends DB_Model{
 
     }
 
+
+
+
+    /*
+    Block Trader Balancer BTC
+    */
+    private function blockTradeAltBalancer($symbol, $balancer){
+        $this->db->where("users_id", $this->login_id);
+        $this->db->where("alt_symbol", $symbol);
+        $getbalancerTarget = $this->db->get("wallet_alt")->row();
+
+        
+        /*
+            Check Balancer
+        */
+        if(!$getbalancerTarget || (float)$getbalancerTarget->alt_trade_avalible < $balancer) return false;
+
+        /*
+        Query Block
+        */
+
+        $total = (float)$getbalancerTarget->alt_trade_avalible - (float)$balancer;
+        $blockTotal = (float)$getbalancerTarget->alt_block_balancer + (float)$balancer;
+
+        $arv = [
+            "alt_trade_avalible" => $total,
+            "alt_block_balancer" => $blockTotal,
+            "updated"   =>  date("Y-m-d h:i:s")
+        ];
+        $target = $this->db->update("wallet_alt",$arv,["alt_id" => $getbalancerTarget->alt_id]);
+        if($target){
+            return true;
+        }
+        return false;
+    }
+
     /*
     Block Trader Balancer BTC
     */
@@ -153,11 +242,11 @@ class Trade_Model extends DB_Model{
         $this->db->where("users_id", $this->login_id);
         $getbalancerTarget = $this->db->get("wallet_btc")->row();
 
-
+        
         /*
             Check Balancer
         */
-        if((float)$getbalancerTarget->btc_trade_avalible < $balancer) return false;
+        if(!$getbalancerTarget || (float)$getbalancerTarget->btc_trade_avalible < $balancer) return false;
 
         /*
         Query Block
@@ -217,6 +306,7 @@ class Trade_Model extends DB_Model{
         /*
         Get Blancer
         Target Blancer BTC Buyer
+        Query to target form user
         */
         $target = false;
         if($target_form_user){
@@ -240,6 +330,7 @@ class Trade_Model extends DB_Model{
 
         /*
         Update BTC for Seller
+        Query to users_id
         */
         if($target){
             $this->db->where("users_id", $users_id);
